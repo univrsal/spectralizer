a796df6e868f4f742cc91ae65a9a4a19b76ea646
univrsal <univrsal@protonmail.ch>
2020-02-07 00:11:05 +0100

    format2

--- src/spectralizer.cpp
+++ src/spectralizer.cpp
@@ -23,6 +23,11 @@ OBS_DECLARE_MODULE()
 
 OBS_MODULE_USE_DEFAULT_LOCALE("spectralizer", "en-US")
 
+MODULE_EXPORT const char *obs_module_description(void)
+{
+	return "Spectrum visualizer";
+}
+
 bool obs_module_load()
 {
 	source::register_visualiser();
